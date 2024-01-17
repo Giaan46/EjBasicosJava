071cc9f2fb848fdf2bbcadcce5bb00c5f764e1f5
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-01-17 19:22:03 +0100

    Update tp2Replit.java
    Este programa en Java encuentra y muestra los números primos en el rango del 2 al 100. Utiliza un enfoque de verificación de primos al iterar sobre los números en el rango y verificar si cada número es divisible por algún otro número menor. La salida es una lista de los números primos encontrados en el rango especificado.

--- tp2Replit.java
+++ tp2Replit.java
@@ -2,7 +2,7 @@ package TpReplit;
 
 import java.util.Scanner;
 
-public class tp2Replit {
+public class NumerosPrimos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
