f31e04a330ff579936cefb0dcb80d254ead33efc
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-01-17 19:17:01 +0100

    Add files via upload

---
+++ tp2Replit.java
@@ -0,0 +1,76 @@
+package TpReplit;
+
+import java.util.Scanner;
+
+public class tp2Replit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = 100;
+        System.out.println(" Los numero primos del 2 al 100 son : " );
+        while (true) {
+
+
+            if (n > 0) {
+                for (int i = 2; i <= n; i++) {
+
+                    int creciente = 2;
+                    boolean esPrimo = true;
+
+                    while (esPrimo && creciente < i) {
+
+                        if (i % creciente == 0) {
+                            esPrimo = false;
+
+                        } else {
+
+                            creciente++;
+
+                        }
+
+
+                    }
+                    if (esPrimo) {
+
+                        System.out.println(i);
+
+
+                    }
+                }
+                {
+                    break;
+                }
+
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
