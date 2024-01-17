85ec194dbe5a8254afc5e9690a8b89ae465efc68
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-01-17 19:17:20 +0100

    Add files via upload

---
+++ tp3Replit.java
@@ -0,0 +1,34 @@
+package TpReplit;
+
+import java.util.Scanner;
+
+public class tp3Replit {
+    public static void main(String[] args) {
+        String may;
+        String min;
+        char punto = '.';
+        System.out.println(punto + "hola mundo" );
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
